e0c209206d93d9d8570b46b0d6257f0e1d230f40
harryg02 <94257270+harryg02@users.noreply.github.com>
2025-11-22 15:33:19 -0500

    update video.js

--- js/video.js
+++ js/video.js
@@ -60,3 +60,13 @@ document.querySelector("#slider").addEventListener("input", function() {
 	video.volume = this.value / 100
 	console.log("Current Video volume is " + video.volume);
 });
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+	console.log("added old school filter");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+	console.log("removed old school filter");
+});
